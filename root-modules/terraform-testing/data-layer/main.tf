3984460939c64e4a9df550c84f73c925eabdbf5d
Mark O'Keeffe <mark.okeeffe@digistorm.com>
2023-10-03 13:11:06 +0100

    Attach DigistormInstanceProfile policy to AWSSystemsManagerDefaultEC2InstanceManagementRole

--- root-modules/terraform-testing/data-layer/main.tf
+++ root-modules/terraform-testing/data-layer/main.tf
@@ -57,43 +57,6 @@ resource "aws_iam_policy" "instance_profile_policy" {
     ssm_output_s3_bucket_name = local.ssm_output_s3_bucket_name
   })
 }
-# Create IAM Role - Assume Role Policy for EC2
-#
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy_document
-data "aws_iam_policy_document" "assume_role_ec2" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = [
-        "ec2.amazonaws.com"
-      ]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-# Create IAM Role
-#
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
-resource "aws_iam_role" "instance_profile_role" {
-  name               = "DigistormInstanceProfile"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_ec2.json
-}
-# Attach Policies to Role
-#
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
-resource "aws_iam_role_policy_attachment" "instance_profile_attach" {
-  role       = aws_iam_role.instance_profile_role.name
-  policy_arn = aws_iam_policy.instance_profile_policy.arn
-}
-# Create Instance Profile for Role
-#
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_instance_profile
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "DigistormInstanceProfile"
-  role = aws_iam_role.instance_profile_role.name
-}
 
 
 # Activate Default Host Management Configuration (DHMC)
@@ -119,16 +82,20 @@ resource "aws_iam_role" "AWSSystemsManagerDefaultEC2InstanceManagementRole" {
   name               = "AWSSystemsManagerDefaultEC2InstanceManagementRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_ssm.json
 }
-# Attache Policy to Role
+# Attache Policies to Role
 #
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 resource "aws_iam_role_policy_attachment" "AmazonSSMManagedEC2InstanceDefaultPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy"
   role       = aws_iam_role.AWSSystemsManagerDefaultEC2InstanceManagementRole.name
 }
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedEC2InstanceDefaultPolicy" {
+  policy_arn = aws_iam_policy.instance_profile_policy.arn
+  role       = aws_iam_role.AWSSystemsManagerDefaultEC2InstanceManagementRole.name
+}
 resource "aws_iam_role_policy_attachment" "instance_profile_attach_ssm_patch" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMPatchAssociation"
-  role       = aws_iam_role.instance_profile_role.name
+  role       = aws_iam_role.AWSSystemsManagerDefaultEC2InstanceManagementRole.name
 }
 # Create SSM Service Setting
 #
